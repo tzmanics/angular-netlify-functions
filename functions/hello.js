a99f05cfb8b56b8f48fb1312700a9db68eab32c5
tzmanics <tzmanics@gmail.com>
2021-01-18 17:42:05 -0500

    fixes log typo

--- functions/hello.js
+++ functions/hello.js
@@ -1,8 +1,8 @@
 exports.handler = async (event, context) => {
-  console.log("deploy-building");
-  console.log("event", event);
-  console.log("context", context);
-  console.log("Hello Angular Background o(*ﾟ∇ﾟ)ﾉ");
+  console.log('deploy-building');
+  console.log('event', event);
+  console.log('context', context);
+  console.log('Hello Angular World o(*ﾟ∇ﾟ)ﾉ');
   return {
     statusCode: 200,
     body: JSON.stringify({
