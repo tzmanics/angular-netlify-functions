89849743d354185a078c306b04773334d8059c2a
tzmanics <tzmanics@gmail.com>
2021-01-22 23:09:49 -0500

    specify functions older + separate logs

--- functions/hello.js
+++ functions/hello.js
@@ -2,8 +2,8 @@ exports.handler = async (event, context) => {
   const location = event.queryStringParameters.location || 'home';
 
   console.log('Hello Angular World o(*ﾟ∇ﾟ)ﾉ');
-  console.log(`Here is the event info: ${JSON.stringify(event)}`);
-  console.log(`Here is the context info: ${JSON.stringify(context)}`);
+  console.log(`\nHere is the event info: ${JSON.stringify(event)}`);
+  console.log(`\nHere is the context info: ${JSON.stringify(context)}`);
 
   return {
     statusCode: 200,
