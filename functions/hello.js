42463a917286944f183b4a6777b90ee9bcb015e1
tzmanics <tzmanics@gmail.com>
2021-01-22 22:26:30 -0500

    switch to simple example

--- functions/hello.js
+++ functions/hello.js
@@ -1,10 +1,12 @@
 exports.handler = async (event, context) => {
+  const location = event.queryStringParameters.location || 'home';
+
   console.log('Hello Angular World o(*ﾟ∇ﾟ)ﾉ');
+  console.log(`Here is the event info: ${JSON.stringify(event)}`);
+  console.log(`Here is the context info: ${JSON.stringify(context)}`);
+
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      event: event,
-      context: context,
-    }),
+    body: `Ng phone ${location}!`,
   };
 };
