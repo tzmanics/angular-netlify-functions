6ce3a5c608b93eff01180437fb05c6cf7dc726cb
tzmanics <tzmanics@gmail.com>
2021-01-22 21:48:41 -0500

    changes return info

--- functions/hello-background.js
+++ functions/hello-background.js
@@ -1,12 +1,10 @@
 exports.handler = async (event, context) => {
-  console.log("deploy-building");
-  console.log("event", event);
-  console.log("context", context);
-  console.log("Hello Angular Background o(*ﾟ∇ﾟ)ﾉ");
+  console.log('Hello Angular Background o(*ﾟ∇ﾟ)ﾉ');
   return {
     statusCode: 200,
     body: JSON.stringify({
-      data: true,
+      event: event,
+      context: context,
     }),
   };
 };
--- functions/hello.js
+++ functions/hello.js
@@ -1,12 +1,10 @@
 exports.handler = async (event, context) => {
-  console.log('deploy-building');
-  console.log('event', event);
-  console.log('context', context);
   console.log('Hello Angular World o(*ﾟ∇ﾟ)ﾉ');
   return {
     statusCode: 200,
     body: JSON.stringify({
-      data: true,
+      event: event,
+      context: context,
     }),
   };
 };
